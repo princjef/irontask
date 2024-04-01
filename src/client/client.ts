8f845dc4cfc2cc1cade097f42bcbf2cb489730a5
Ethan <ethanperry@microsoft.com>
2024-04-01 13:12:11 -0700

    Remove redundant copyright statements

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,11 +3,6 @@
  * Licensed under the MIT License.
  */
 
-/* !
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import { SqlQuerySpec } from '@azure/cosmos';
 import { ChainedTokenCredential } from '@azure/identity';
 import { v4 as uuid } from 'uuid';
